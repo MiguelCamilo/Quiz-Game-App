7b865adebc2c2c53f4e8ff6e5ba2c93f0bae4053
Miguel Camilo <miguelcamilo48@gmail.com>
2023-03-30 16:51:39 -0400

    Quiz Card component UI complete

--- src/components/QuizCard/QuizCard.jsx
+++ src/components/QuizCard/QuizCard.jsx
@@ -1,9 +1,20 @@
+import { data } from "../../data/quiz_data";
+
 export default function QuizCard() {
 	return (
 		<>
-			<h2 className="text-white text-4xl font-black font-mono mr-[12rem] mb-5">Quiz Game</h2>
+			<h2 className="text-white text-4xl font-black font-mono mr-[10rem] md:mr-[13rem] mb-5">Random Quiz Game</h2>
 			{/* quiz card container */}
-			<div className="bg-white h-[25rem] w-[25rem] rounded-xl"></div>
+			<div className="bg-white h-[38rem] w-[23rem] md:h-[40rem] md:w-[35rem] rounded-xl">
+                {/* questions map */}
+            </div>
 		</>
 	);
 }
+
+// {!data 
+//     ? <p>Loading...</p>
+//     : data.map((element) => (
+//         <p>{element.question}</p>
+//     ))
+// }
---
+++ src/data/quiz_data.js
@@ -0,0 +1,34 @@
+
+export const data = [
+	{
+		id: "1",
+		question: "What is the capital of U.S?",
+		options: [
+			{ answer: "New York City", isCorrect: false },
+			{ answer: "Washington D.C", isCorrect: true },
+			{ answer: "Miami", isCorrect: false },
+			{ answer: "Chicago", isCorrect: false },
+		],
+	},
+	{
+		id: "2",
+		question: "Whos is the first president of U.S?",
+		options: [
+			{ answer: "John Adams", isCorrect: false },
+			{ answer: "Obama", isCorrect: false },
+			{ answer: "George Washignton", isCorrect: true },
+			{ answer: "Biden", isCorrect: false },
+		],
+	},
+	{
+		id: "3",
+		question: "How many bones do sharks have?",
+		options: [
+			{ answer: "75", isCorrect: false },
+			{ answer: "13", isCorrect: false },
+			{ answer: "0", isCorrect: true },
+			{ answer: "It has to be --->  5", isCorrect: false },
+		],
+	},
+    
+];
