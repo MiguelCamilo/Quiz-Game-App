cc22fcc9e6c7290106a922419d209805d1dae99f
Miguel Camilo <miguelcamilo48@gmail.com>
2023-04-05 19:05:38 -0400

    Commit before adding google auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Quiz from './components/Quiz/Quiz'
+import Login from './pages/Login'
+
 import './App.css'
 
 function App() {
