1b2c44271061d234c2b43106c6f04a31bc5b709b
Miguel Camilo <miguelcamilo48@gmail.com>
2023-05-08 23:00:25 -0400

    Added href link to footer

--- src/components/QuizCard/Footer.jsx
+++ src/components/QuizCard/Footer.jsx
@@ -2,7 +2,10 @@ export default function Footer() {
 	const current_date = new Date().getFullYear();
 	return (
 		<p className="text-[12px] text-center text-white mb-2 google-font">
-			{current_date} Created by <span className="text-[#FF9800]">Miguel Camilo</span>
+			{current_date} See more at: {' '}
+			<a href="https://miguelcamilo.dev" target="_blank">
+				<span className="text-[#FF9800]">Miguel Camilo</span>
+			</a>
 		</p>
 	);
 }
